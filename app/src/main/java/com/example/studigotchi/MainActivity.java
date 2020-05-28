c529914a92bc3829fc6537ba99b44f193d073462
lnbs97 <45437638+lnbs97@users.noreply.github.com>
2020-05-28 21:21:09 +0200

    ImageView für Studi hinzugefügt Platzhalter-Grafiken für Studi idle und lernen hinzugefügt Lernen Button hinzugefügt Platzhalter Funktion für Lernen hinzugefügt

--- app/src/main/java/com/example/studigotchi/MainActivity.java
+++ app/src/main/java/com/example/studigotchi/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.studigotchi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.icu.text.RelativeDateTimeFormatter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.DateFormat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mStudiImageView;
+    private Button mLearnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +28,19 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textViewDate = findViewById(R.id.textView_date);
         textViewDate.setText(currentDate);
+
+        mStudiImageView = findViewById(R.id.imageView_studi);
+        mLearnButton = findViewById(R.id.button_learn);
+
+        mLearnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                learn();
+            }
+        });
+    }
+
+    private void learn() {
+        mStudiImageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_studi_learning));
     }
 }
