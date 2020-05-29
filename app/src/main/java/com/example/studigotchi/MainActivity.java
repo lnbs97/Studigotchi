3a5309cd0b728cdeb6d6d618fec05faf45f6068c
Jonas Feige <jonasfeige@gmx.de>
2020-05-29 14:04:41 +0200

    CurrentDate wird als Timestamp in Sharedprefs bei Schließen der App gespeichert Beim Öffnen der App werden die Daten der Sharedprefs aufgerufen und in timestamp gespeichert
    es gibt ein warning bezüglich des "commit()" beim Speichern der SharedPrefs:
    Warning:(84, 9) Consider using `apply()` instead; `commit` writes its data to persistent storage immediately, whereas `apply` will handle it in the background
    
    aber ich denke, für unsere Anwendung ist commit() hier eigentlich richtig. Ich verstehe aber auch nicht so ganz, was hier mit "handle it in the background" genau gemeint ist.

--- app/src/main/java/com/example/studigotchi/MainActivity.java
+++ app/src/main/java/com/example/studigotchi/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.studigotchi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.SharedPreferences;
 import android.icu.text.RelativeDateTimeFormatter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,17 +20,12 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView mStudiImageView;
     private Button mLearnButton;
-
+    private static final String TAG = "MainActivity";
+    private String timestamp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Calendar calendar = Calendar.getInstance();
-        String currentDate = DateFormat.getDateTimeInstance().format(calendar.getTimeInMillis());
-
-        TextView textViewDate = findViewById(R.id.textView_date);
-        textViewDate.setText(currentDate);
 
         mStudiImageView = findViewById(R.id.imageView_studi);
         mLearnButton = findViewById(R.id.button_learn);
@@ -38,6 +36,22 @@ public class MainActivity extends AppCompatActivity {
                 learn();
             }
         });
+
+        Log.i(TAG, "onCreate");
+    }
+
+    //beim Öffnen der App wird timestamp ausgelesen und timestamp gespeichert
+    protected void onResume() {
+        super.onResume();
+        //Shared Prefs Datei öffnen
+        SharedPreferences mySPR = getSharedPreferences("mySPRFILE", 0);
+        //SharedPref auslesen und timestamp schreiben
+        timestamp = mySPR.getString("myDate", "date");
+        System.out.println(timestamp);
+
+        //timestamp ausgeben (Nur als Nachweis, dass es funktioniert)
+        TextView textView = findViewById(R.id.textView_date);
+        textView.setText(timestamp);
     }
 
     /**
@@ -47,5 +61,27 @@ public class MainActivity extends AppCompatActivity {
     private void learn() {
         // Bild ändern auf Lernen Bild
         mStudiImageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_studi_learning));
+    }
+
+    //Beim Pausieren/Schließen der App wird ein aktueller timestamp in den SharedPrefs gespeichert.
+    protected void onPause(){
+        super.onPause();
+
+        //Shared Prefs Datei öffnen
+        SharedPreferences mySPR = getSharedPreferences("mySPRFILE", 0);
+
+        //Editor Klasse initialisieren
+        SharedPreferences.Editor sprEditor = mySPR.edit();
+
+        //CurrentDate als String aus Kalender abgreifen
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateTimeInstance().format(calendar.getTimeInMillis());
+
+        //CurrentDate in mySPR speichern
+        sprEditor.putString("myDate", currentDate);
+
+        //Speichern
+        sprEditor.commit();
+
     }
 }
